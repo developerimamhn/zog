3985167c9ab153ed89aea280e959c0d911093e82
Imam Hossen <imam.frontend404@gmail.com>
2026-06-04 17:36:43 +0600

    margenayeem

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={
           <div className="w-full h-auto bg-[#F8F8F8]">
             <Navbar />
-            <div className="grid grid-cols-12 mx-auto justify-between gap-2.5 sm:gap-3 md:gap-4 lg:gap-5 xl:gap-6 2xl:gap-7.5 pt-10 container">
+            <div className="md:grid grid-cols-12 mx-auto justify-between gap-2.5 sm:gap-3 md:gap-4 lg:gap-5 xl:gap-6 2xl:gap-7.5 pt-10 container">
               <div className="col-span-2 lg:col-span-2">
                 <Sidebar />
               </div>
